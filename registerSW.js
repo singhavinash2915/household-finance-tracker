ef1c1f56b988166b1eeabdf5304ee7b09cc5dbe7
singhavinash2915 <singhavinash2915@users.noreply.github.com>
2026-09-09 03:13:58 +0000

    deploy: 6891b2848f64fe6a8a93239c90d0b3a3cbecfe69

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/household-finance-tracker/sw.js', { scope: '/household-finance-tracker/' })})}
